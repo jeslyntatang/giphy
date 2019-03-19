1d6530dcf8a70f2ef060366e781a554ad5848870
Jeslyn Tatang <jeslyn.tatang@gmail.com>
2019-03-19 17:58:04 +0000

    email button

--- js/script.js
+++ js/script.js
@@ -16,9 +16,12 @@ $("#search-button").click(function(){
       
   });
   
-  $(".email-button").click(function () {
-    var email = $(".email-input").val();
+ $("#email-button").click(function () {
+     var email = $("#email-term").val();
+    window.open('mailto:'+email+'?subject=subject&body=body');
   });
+  
+  
   
   console.log(searchTerm);
   
